7079f88ab44cf3f5d2b8209472c5ae5d2cf5b976
Stephen Young <young.steveo@gmail.com>
2016-08-03 16:38:07 -0400

    Renamed instanceProvider to instanceFactory

--- src/Bottle/instance-factory.js
+++ src/Bottle/instance-factory.js
@@ -1,15 +1,15 @@
 /**
- * Register an instance provider inside a generic provider.
+ * Register an instance factory inside a generic factory.
  *
  * @param {String} name - The name of the service
  * @param {Function} Factory - The factory function, matches the signature required for the
  * `factory` method
  * @return Bottle
  */
-var instanceProvider = function instanceProvider(name, Factory) {
-  return factory.call(this, name, function GenericInstanceFactory(container) {
-    return {
-      instance : Factory.bind(null, container)
-    };
-  });
+var instanceFactory = function instanceFactory(name, Factory) {
+    return factory.call(this, name, function GenericInstanceFactory(container) {
+        return {
+            instance : Factory.bind(Factory, container)
+        };
+    });
 };
