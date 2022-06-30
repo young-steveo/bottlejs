2a94d3891a2b6d807e4be9aad8a11d7b9462a652
Stephen Young <young.steveo@gmail.com>
2022-06-29 21:44:54 -0400

    Add Prittier and TSLint tools.

--- src/bottle.ts
+++ src/bottle.ts
@@ -1,14 +1,14 @@
-const instances : Record<string,Bottle> = {}
+const instances: Record<string, Bottle> = {}
 
 export class Bottle {
-    public static pop(name: string|undefined): Bottle {
-        if (typeof name !== 'string') {
-            return new Bottle()
-        }
-        let instance = instances[name]
-        if (!instance) {
-            instances[name] = instance = new Bottle()
-        }
-        return instance
+  public static pop(name: string | undefined): Bottle {
+    if (typeof name !== 'string') {
+      return new Bottle()
     }
+    let instance = instances[name]
+    if (!instance) {
+      instances[name] = instance = new Bottle()
+    }
+    return instance
+  }
 }
